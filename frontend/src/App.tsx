bfd3ab304f697175af5ececf0f075a7c3bebb43c
AkankshaNarula <64724811+AkankshaNarula@users.noreply.github.com>
2024-09-23 00:04:10 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./components/auth/login";
 import Register from "./components/auth/register";
 import Header from "./components/header";
-import Home from "./components/home/index";
+import Home, { MinistryHome } from "./components/page.tsx";
 import Dash_Home from "./components/Dash_home";
 import { AuthProvider } from "./components/AuthProvider";
 
@@ -14,12 +14,13 @@ const App: React.FC = () => {
         <Header />
         <div className="w-full h-screen flex flex-col">
           <Routes>
-            <Route path="/" element={<Dash_Home />} />
+            <Route path="/" element={<MinistryHome />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
             <Route path="/home" element={<Home />} />
             <Route path="/Dash_home" element={<Dash_Home />} />
-            <Route path="*" element={<Login />} /> {/* Fallback route for unknown paths */}
+            {/* Fallback to MinistryHome if the route is unknown */}
+            <Route path="*" element={<Navigate to="/" />} />
           </Routes>
         </div>
       </BrowserRouter>
